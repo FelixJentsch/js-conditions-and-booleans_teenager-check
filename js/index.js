d7731adb6c7bcb95a7128ff6b52f466b51ab9292
FelixJentsch <FelixJentsch91@googlemail.com>
2023-11-15 16:24:46 +0100

    solved challenge

--- js/index.js
+++ js/index.js
@@ -7,7 +7,12 @@ button.addEventListener("click", () => {
   // Beware: input values are *always* strings!
   const age = Number(input.value);
 
-  // Exercise:
-  // Use conditions and the logical AND operator to write
-  // "You are a teen." or "You are not a teen." into the output.
+  if (age >= 13 && age <= 19) {
+    output.textContent = "You are a teen.";
+  } else {
+    output.textContent = "You are not a teen.";
+  }
 });
+// Exercise:
+// Use conditions and the logical AND operator to write
+// "You are a teen." or "You are not a teen." into the output.
